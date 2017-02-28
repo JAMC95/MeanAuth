833a966486c18764bf579dfb38e9be4f026b438e
Jose Antonio Moral <joxe.bailen@gmail.com>
2017-02-28 16:19:58 +0100

    Added login module with the comand 'ng g component login'

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,

b8c8053bcaf75ec00a66fd54a28fb3a638a58fe3
Jose Antonio Moral <joxe.bailen@gmail.com>
2017-03-11 11:01:18 +0100

    Dashboard template and good practices with Express in app.js

--- app.js
+++ app.js
@@ -48,6 +48,10 @@ app.get('/', (req, res) => {
     res.send('Invalid endpoint')
 })
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
 // Start Server
 app.listen(port, () => {
     console.log('Sever stared on port '+port)

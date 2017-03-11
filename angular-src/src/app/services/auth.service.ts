c86ca8274b3ae320bce98c251cada0977824c660
Jose Antonio Moral <joxe.bailen@gmail.com>
2017-03-11 09:38:01 +0100

    Added getProfile and loadToken function This is the logic for create the Profile webpage

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -23,6 +23,15 @@ export class AuthService {
       .map(res => res.json());
   }
   
+  getProfile () {
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/users/profile', {headers: headers})
+      .map(res => res.json());
+  }
+
   storeUserData (token, user) {
     localStorage.setItem('id_token', token);
     localStorage.setItem('user', JSON.stringify(user));
@@ -31,6 +40,11 @@ export class AuthService {
 
   }
   
+loadToken () {
+  const token = localStorage.getItem('id_token');
+  this.authToken = token;
+}
+
   logout () {
     this.authToken = null;
     this.user = null; 

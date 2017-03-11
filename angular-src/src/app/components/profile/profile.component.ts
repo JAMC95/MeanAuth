8d5cf701eaac3de4c382fbedaf56a9e73153dbbc
Jose Antonio Moral <joxe.bailen@gmail.com>
2017-03-11 10:06:46 +0100

    Added html to show a profile in /profile/ Fixed a bug in auth (get to post)

--- angular-src/src/app/components/profile/profile.component.ts
+++ angular-src/src/app/components/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -8,6 +8,7 @@ import { Router } from '@angular/router'
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+ 
   user:Object;
 
   constructor(
@@ -19,6 +20,7 @@ export class ProfileComponent implements OnInit {
   ngOnInit() {
     this.authService.getProfile().subscribe(profile => {
       this.user = profile.user;
+      
     },
     err => {
       console.log(err);
